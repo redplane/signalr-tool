ed5dfe2b4af2e0f3669f11d6d62ff42a14e9c60c
Linh Nguyen <linh.nguyen94@outlook.com.vn>
2022-05-12 11:11:01 +0700

    Initial commit

---
+++ SignalrPublisherTool/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Events;
+
+var hubUrl = string.Empty;
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+    .WriteTo.File(Directory.GetCurrentDirectory(), LogEventLevel.Debug)
+    .CreateLogger();
+
+// Hub name populate.
+while (true)
+{
+    Console.Write("Hub url: ");
+    hubUrl = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(hubUrl))
+    {
+        Console.WriteLine("Invalid hub url. Please try again");
+        continue;
+    }
+
+    break;
+}
+
---
+++ SignalrRecipientTool/Program.cs
@@ -0,0 +1,154 @@
+﻿// using Microsoft.AspNetCore.SignalR.Client;
+// using Newtonsoft.Json;
+// using Serilog;
+// using Serilog.Events;
+//
+// var hubUrl = string.Empty;
+//
+//
+// Log.Logger = new LoggerConfiguration()
+//         .MinimumLevel.Debug()
+//         .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+//         .WriteTo.File(Directory.GetCurrentDirectory(), LogEventLevel.Debug)
+//     .CreateLogger();
+//
+// // Hub name populate.
+// while (true)
+// {
+//     Console.Write("Hub url: ");
+//     hubUrl = Console.ReadLine();
+//
+//     if (string.IsNullOrEmpty(hubUrl))
+//     {
+//         Console.WriteLine("Invalid hub url. Please try again");
+//         continue;
+//     }
+//
+//     break;
+// }
+//
+// var methodNames = new HashSet<string>();
+// while (true)
+// {
+//     Console.Write("Method name (QUIT for stop inputting): ");
+//     var methodName = Console.ReadLine();
+//     if (string.IsNullOrEmpty(methodName))
+//     {
+//         Console.WriteLine("Method name cannot be empty");
+//         continue;
+//     }
+//
+//     if ("QUIT".Equals(methodName, StringComparison.InvariantCultureIgnoreCase))
+//     {
+//         break;
+//     }
+//
+//     methodNames.Add(methodName);
+// }
+//
+// var hubConnection = new HubConnectionBuilder()
+//     .WithUrl(hubUrl)
+//     .WithAutomaticReconnect()
+//     .Build();
+//
+// if (!methodNames.Any())
+// {
+//     Console.WriteLine("No method name is defined. Application will be stopped.");
+//     return;
+// }
+//
+// foreach (var methodName in methodNames)
+// {
+//     hubConnection.On<object>(methodName, (message) =>
+//     {
+//         Log.Logger.Debug(JsonConvert.SerializeObject(message));
+//         return Task.CompletedTask;
+//     });
+//     
+//     Console.WriteLine($"Connected to method {methodName}");
+// }
+//
+// Console.WriteLine("Starting signalr client...");
+// hubConnection.StartAsync().Wait();
+// Console.WriteLine("Signalr client started !");
+//
+// Console.ReadLine();
+//
+
+
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Events;
+
+var hubUrl = string.Empty;
+
+
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+        .WriteTo.File(Directory.GetCurrentDirectory(), LogEventLevel.Debug)
+    .CreateLogger();
+
+// Hub name populate.
+while (true)
+{
+    Console.Write("Hub url: ");
+    hubUrl = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(hubUrl))
+    {
+        Console.WriteLine("Invalid hub url. Please try again");
+        continue;
+    }
+
+    break;
+}
+
+var methodNames = new HashSet<string>();
+while (true)
+{
+    Console.Write("Method name (QUIT for stop inputting): ");
+    var methodName = Console.ReadLine();
+    if (string.IsNullOrEmpty(methodName))
+    {
+        Console.WriteLine("Method name cannot be empty");
+        continue;
+    }
+
+    if ("QUIT".Equals(methodName, StringComparison.InvariantCultureIgnoreCase))
+    {
+        break;
+    }
+
+    methodNames.Add(methodName);
+}
+
+var hubConnection = new HubConnectionBuilder()
+    .WithUrl(hubUrl)
+    .WithAutomaticReconnect()
+    .Build();
+
+if (!methodNames.Any())
+{
+    Console.WriteLine("No method name is defined. Application will be stopped.");
+    return;
+}
+
+foreach (var methodName in methodNames)
+{
+    hubConnection.On<object>(methodName, (message) =>
+    {
+        Log.Logger.Debug(JsonConvert.SerializeObject(message));
+        return Task.CompletedTask;
+    });
+    
+    Console.WriteLine($"Connected to method {methodName}");
+}
+
+Console.WriteLine("Starting signalr client...");
+hubConnection.StartAsync().Wait();
+Console.WriteLine("Signalr client started !");
+
+Console.ReadLine();
+
